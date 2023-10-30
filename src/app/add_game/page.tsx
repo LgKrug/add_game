70ecc94d09fa725f793a5d234e96fd5fa4f8a78e
Luiz Gustavo Krug <luizg.krug@gmail.com>
2023-10-30 00:04:48 -0300

    fix page css

---
+++ src/app/add_game/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+export default function Add_game() {
+    return (
+        
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,23 @@
 "use client";
-
-import styles from "../app/styles.module.css"
 import Image from "next/image";
-import Button from "../components/Button";
+import Link from "next/link";
+import styles from "./page.module.css";
 import logo from "public/logoMais.png";
+import Button from "../components/Button";
+import { useRouter } from "next/navigation";
 
-import { useRouter } from "next/navigation"; // Alterei de "next/navigation" para "next/router" - utilize o pacote correto
-
-const HomePage = () => {
+export default function Home() {
   const router = useRouter();
 
   return (
     <div className={styles.container}>
-      <div className="mb-8">
-        <Image src={logo} alt="project logo" width={150} height={150} />
+      <div className={styles.logo}>
+        <Image src={logo} alt="project logo" width={128} height={128} />
       </div>
-      <h1 className="text-4xl font-extrabold text-center text-blue-500">
-        Bem-vindo ao Seu Jogo de Soma!
-      </h1>
-      <div className="flex space-x-4 mt-8">
-        <div className="px-6 py-3 bg-blue-500 text-white rounded-lg flex items-center">
-          <Button onClick={() => router.push("/login")} label="Entrar" />
-        </div>
-        <div className="px-6 py-3 bg-green-500 text-white rounded-lg flex items-center">
-          <Button onClick={() => router.push("/register")} label="Cadastro" />
-        </div>
+      <div className={styles.buttons}>
+        <Button onClick={() => router.push("/login")} label="Login" />
+        <Button onClick={() => router.push("/register")} label="Cadastro" />
       </div>
     </div>
   );
-};
-
-export default HomePage;
+}
